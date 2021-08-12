b53e8d4cee53852ed99795bc0b75c967ba7e25b6
jumperstumper <daniieleriksson92@gmail.com>
2021-08-12 14:43:21 +0200

    up

---
+++ WorkOrderSysten/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using InventoryControlSystem.Models;
+using InventoryControlSystem.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryControlSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Register()
+        {
+            RegisterVM registerVM = new RegisterVM();
+            return View(registerVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = registerVM.Email, Email = registerVM.Email, Name = registerVM.FirstName};
+                var result = await _userManager.CreateAsync(user, registerVM.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+
+                    return RedirectToAction("Index", "Home");
+                }
+                AddErrors(result);
+
+            }
+
+            return View(registerVM);
+        }
+
+        private void AddErrors(IdentityResult result) 
+        {
+            foreach(var error in result.Errors) 
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+    }
+}
---
+++ WorkOrderSysten/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WorkOrderSysten.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
